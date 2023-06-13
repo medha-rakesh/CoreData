bfb044055ef41d6dd75056d274ed6eaa2d12514e
medha rakesh <medha.rakesh08@gmail.con>
2023-06-13 08:28:58 -0700

    Initial Commit

---
+++ CoraData/CoraDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoraDataApp.swift
+//  CoraData
+//
+//  Created by MahaPeriva on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CoraDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
